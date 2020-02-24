2b621176aac0b394a597c2b35c9cf4444ea05795
scZhengChao <15082810674@163.com>
2020-02-24 11:14:18 +0800

    zc

--- nodeBE/routes/webjssdk/logs.js
+++ nodeBE/routes/webjssdk/logs.js
@@ -4,28 +4,46 @@ let logs = (req,res,next)=>{
     data.pop()
     let arr = data.map(item=>JSON.parse(item))
     console.log(arr)
+
+    // {
+    //     project: 'BOTA',
+    //     sdk_version: '1.0',
+    //     project_env: 'test',
+    //     time: 1582511775401,
+    //     page_session: '4hola3oy3a1-1582511527060',
+    //     distinct_id: '5931',
+    //     href_url: 'http://localhost:8080/#/',
+    //     event: 'visible',
+    //     upload_type: 'PV',
+    //     device_name: 'PC',
+    //     browser_name: 'chrome',
+    //     browser_version: '80.0.3987.87'
+    //   }
+    
     let seqarr = []
     arr.forEach(item => {
         seqarr.push({
-            distinct_id:item.customerUnqKey,
-            time:item.happenedTime,
+            distinct_id:item.distinct_id,
+            time:item.time,
             type:'track',
-            event:item.pageEvent,
-            project:item.webId,
-            herf_url:item.hrefUrl,
-            upload_type:item.uploadType,
-            device_name:item.deviceName,
-            sdk_version:item.customerUnike || 'hah',
-            browser_name:item.browserName,
-            browser_version:item.browserVersion,
-            time_on_page:item.onPageTime
+            event:item.event,
+            project:item.project,
+            href_url:item.href_url,
+            upload_type:item.upload_type,
+            device_name:item.device_name,
+            sdk_version:item.sdk_version,
+            browser_name:item.browser_name,
+            browser_version:item.browser_version,
+            time_on_page:item.time_on_page,
+            page_session:item.page_session,
+            project_env:item.project_env
         })
     });
     sqlLogs.bulkCreate(seqarr).then(results=>{
-        res.send({code:100000,msg:'success'})
+        res.send({code:100001,msg:'success'})
     }).catch(err=>{
         // console.log('err logs:',err)
-        res.send({code:10001,msg:err})
+        res.send({code:100000,msg:err})
     })
    
     // res.send({code:10001,msg:'success'})
--- nodeBE/routes/webjssdk/users.js
+++ nodeBE/routes/webjssdk/users.js
@@ -1,16 +1,14 @@
 let userLogs = require('../../sequelize/relation').users
 let customersinfo = (req,res,next)=>{
     let data = JSON.parse(req.body.data).userInfo
-    console.log(data)
     userLogs.create({
-        name:data.name,
         id:data.id,
     }).then(results=>{
         res.send({code:100001,msg:results})
     }).catch(err=>{
         console.log('err user:',err)
         logger.error(error)
-        res.send({code:1,msg:err})
+        res.send({code:100000,msg:err})
     })    
 }
 module.exports = customersinfo
